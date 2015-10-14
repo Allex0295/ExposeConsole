757a7d596e9cbf68a73f245b3c5b463b3802f190
dumesnil alexis <alexis.dumesnil02@gmail.com>
2015-10-14 22:28:44 +0200

    Mise en fonction, fonction tactile

--- source/MainConsole.cpp
+++ source/MainConsole.cpp
@@ -1,11 +1,22 @@
-//TEST GITHUB
 #include <nds.h>
 #include <stdio.h>
-//Commande a ajouter au debut pour compiler
-//	export DEVKITPRO=/usr/local/devkitPro
-//	export DEVKITARM=/usr/local/devkitPro/devkitARM/
 
-void testInput(){	//Fonction de test des touches
+void touch(){		//Fonction de test de l'écran tactile
+	touchPosition touchXY;
+	int j;
+	for(j=4;j<=20;j++){
+		iprintf("\x1b[%u;12H|",j);
+	}
+	iprintf("\x1b[12;0H------------");
+	iprintf("\x1b[15;0H------------");
+	touchRead(&touchXY);
+	iprintf("\x1b[13;0Hx=%04X,%04X\n", touchXY.rawx, touchXY.px);
+	iprintf("y=%04X,%04X", touchXY.rawy, touchXY.py);
+	if ((touchXY.px>0x7F)&&(touchXY.px!=0x00)) iprintf("\x1b[6;14H -->");
+	if ((touchXY.px<0x7F)&&(touchXY.px!=0x00)) iprintf("\x1b[6;14H <--");
+	if ((touchXY.px==0x00)&&(touchXY.py==0x00)) iprintf("\x1b[6;14H    ");
+}
+void input(){		//Fonction de test des touches
 	scanKeys();
 	if(keysHeld() & KEY_A)
 		iprintf("\x1b[4;0HAppui A");		//Bouton A
@@ -24,41 +35,28 @@ void testInput(){	//Fonction de test des touches
 	else if(keysHeld() & KEY_SELECT)
 		iprintf("\x1b[11;0HAppui SELECT");	//Bouton SELECT
 	else{
-		int i;								//Effaçage zone
+		int i;					//Effaçage zone
 		for(i=4;i<=11;i++){
 			iprintf("\x1b[%u;0H            ",i);
 		}
 	}
 }
-
 void screen(){		//Fonction affichage entete
 	iprintf("\x1b[0;13HINSSET\n");
 	iprintf("PROJET REUTILISATION CONSOLE\n");
 	iprintf("Hello World\n");
-	iprintf("------------O-------------------");
-	int j;
-	for(j=4;j<=20;j++){
-		iprintf("\x1b[%u;12H|",j);
-	}
-	iprintf("\x1b[12;0H------------O");
-	iprintf("\x1b[15;0H------------O");
+	iprintf("--------------------------------");
 }
 
 int main(void) {
 //Initialisation
-	touchPosition touchXY;
 	consoleDemoInit();
 	screen();
 
 //Programme
 	while(1){
-		touchRead(&touchXY);
-		iprintf("\x1b[13;0Hx=%04X,%04X\n", touchXY.rawx, touchXY.px);
-		iprintf("y=%04X,%04X", touchXY.rawy, touchXY.py);
-		if ((touchXY.px>0x90)&&(touchXY.px!=0x00)) iprintf("\x1b[6;14H -->");
-		if ((touchXY.px<0x90)&&(touchXY.px!=0x00)) iprintf("\x1b[6;14H <--");
-		if ((touchXY.px==0x00)&&(touchXY.py==0x00)) iprintf("\x1b[6;14H    ");
-		testInput ();
+		touch();
+		input ();
 	}
 	return 0;
 }
